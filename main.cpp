68150652e907e3d3591ed71cb1d9d932fdddd17d
lithellx <45209073+lithellx@users.noreply.github.com>
2026-02-20 23:32:41 +0300

    Refactor NVIDIA ShadowPlay DRM Bypass with tray icon

--- main.cpp
+++ main.cpp
@@ -14,9 +14,11 @@
 #include <windows.h>
 #include <tlhelp32.h>
 #include <winternl.h>
+#include <shellapi.h>
 #include <stdio.h>
 
 #pragma comment(lib, "ntdll.lib")
+#pragma comment(lib, "shell32.lib")
 
 #define CHECK_INTERVAL 500
 #define BOOT_DELAY     15000
@@ -24,8 +26,21 @@
 #define REG_SUBKEY     "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"
 #define REG_VALUE      "NVBypass"
 
+#define WM_TRAYICON    (WM_USER + 1)
+#define ID_TRAY_SHOW   1001
+#define ID_TRAY_AUTO   1002
+#define ID_TRAY_QUIT   1003
+
 typedef NTSTATUS(WINAPI* pfnNtQIP)(HANDLE, PROCESSINFOCLASS, PVOID, ULONG, PULONG);
 static pfnNtQIP pNtQIP = nullptr;
+
+static NOTIFYICONDATAA nid = {};
+static HWND g_hwnd = nullptr;
+static HWND g_console = nullptr;
+static bool g_consoleVisible = true;
+static bool g_running = true;
+
+// ---- Browsers ----
 
 static const wchar_t* BROWSERS[] =
 {
@@ -48,6 +63,8 @@ static bool IsBrowser(const wchar_t* name)
     }
     return false;
 }
+
+// ---- Command line reader ----
 
 static bool ReadCmdLine(DWORD pid, wchar_t* out, size_t max)
 {
@@ -102,6 +119,8 @@ static bool ReadCmdLine(DWORD pid, wchar_t* out, size_t max)
     return true;
 }
 
+// ---- CDM killer ----
+
 static int KillCDM(bool log)
 {
     int killed = 0;
@@ -145,6 +164,8 @@ static int KillCDM(bool log)
     return killed;
 }
 
+// ---- Auto-start ----
+
 static bool HasAutoStart()
 {
     HKEY k;
@@ -187,6 +208,124 @@ static void ToggleAutoStart()
     }
 }
 
+// ---- Tray icon ----
+
+static void AddTrayIcon(HWND hwnd)
+{
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = hwnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_TRAYICON;
+    nid.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+    strcpy_s(nid.szTip, "NVIDIA ShadowPlay DRM Bypass");
+
+    Shell_NotifyIconA(NIM_ADD, &nid);
+}
+
+static void RemoveTrayIcon()
+{
+    Shell_NotifyIconA(NIM_DELETE, &nid);
+}
+
+static void ShowTrayMenu(HWND hwnd)
+{
+    POINT pt;
+    GetCursorPos(&pt);
+
+    HMENU menu = CreatePopupMenu();
+
+    if (g_consoleVisible)
+        AppendMenuA(menu, MF_STRING, ID_TRAY_SHOW, "Hide");
+    else
+        AppendMenuA(menu, MF_STRING, ID_TRAY_SHOW, "Show");
+
+    AppendMenuA(menu, MF_STRING | (HasAutoStart() ? MF_CHECKED : 0), ID_TRAY_AUTO, "Auto-start");
+    AppendMenuA(menu, MF_SEPARATOR, 0, nullptr);
+    AppendMenuA(menu, MF_STRING, ID_TRAY_QUIT, "Quit");
+
+    SetForegroundWindow(hwnd);
+    TrackPopupMenu(menu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, hwnd, nullptr);
+    DestroyMenu(menu);
+}
+
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_TRAYICON:
+        if (lp == WM_RBUTTONUP || lp == WM_LBUTTONUP)
+            ShowTrayMenu(hwnd);
+        return 0;
+
+    case WM_COMMAND:
+        switch (LOWORD(wp))
+        {
+        case ID_TRAY_SHOW:
+            g_consoleVisible = !g_consoleVisible;
+            ShowWindow(g_console, g_consoleVisible ? SW_SHOW : SW_HIDE);
+            break;
+
+        case ID_TRAY_AUTO:
+            ToggleAutoStart();
+            break;
+
+        case ID_TRAY_QUIT:
+            g_running = false;
+            RemoveTrayIcon();
+            PostQuitMessage(0);
+            break;
+        }
+        return 0;
+
+    case WM_DESTROY:
+        RemoveTrayIcon();
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProcA(hwnd, msg, wp, lp);
+}
+
+static HWND CreateMessageWindow()
+{
+    WNDCLASSA wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = GetModuleHandleA(nullptr);
+    wc.lpszClassName = "NVBypassTray";
+
+    RegisterClassA(&wc);
+
+    return CreateWindowA(wc.lpszClassName, "", 0, 0, 0, 0, 0, HWND_MESSAGE, nullptr, wc.hInstance, nullptr);
+}
+
+// ---- Worker thread ----
+
+static DWORD WINAPI WorkerThread(LPVOID param)
+{
+    bool silent = *(bool*)param;
+    int total = 0;
+
+    while (g_running)
+    {
+        int k = KillCDM(!silent);
+
+        if (k > 0)
+        {
+            total += k;
+
+            if (!silent)
+                printf("Total: %d\n", total);
+        }
+
+        Sleep(CHECK_INTERVAL);
+    }
+
+    return 0;
+}
+
+// ---- Entry ----
+
 int main(int argc, char** argv)
 {
     bool silent = false;
@@ -213,9 +352,12 @@ int main(int argc, char** argv)
     if (GetLastError() == ERROR_ALREADY_EXISTS)
         return 0;
 
+    g_console = GetConsoleWindow();
+
     if (silent)
     {
-        FreeConsole();
+        ShowWindow(g_console, SW_HIDE);
+        g_consoleVisible = false;
         Sleep(BOOT_DELAY);
     }
 
@@ -223,22 +365,27 @@ int main(int argc, char** argv)
     {
         printf("NVIDIA ShadowPlay DRM Bypass\n");
         printf("Auto-start: %s (toggle with --autostart)\n", HasAutoStart() ? "ON" : "OFF");
-        printf("Monitoring CDM processes. Ctrl+C to quit.\n");
+        printf("Monitoring CDM processes. Ctrl+C to quit.\n\n");
     }
 
-    int total = 0;
-    while (true)
+    // Create tray icon
+    g_hwnd = CreateMessageWindow();
+    AddTrayIcon(g_hwnd);
+
+    // Start worker thread
+    HANDLE hThread = CreateThread(nullptr, 0, WorkerThread, &silent, 0, nullptr);
+
+    // Message loop
+    MSG msg;
+    while (GetMessageA(&msg, nullptr, 0, 0))
     {
-        int k = KillCDM(!silent);
-        if (k > 0)
-        {
-            total += k;
-            if (!silent)
-                printf("Total: %d\n\n", total);
-        }
-
-        Sleep(CHECK_INTERVAL);
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
     }
+
+    g_running = false;
+    WaitForSingleObject(hThread, 2000);
+    CloseHandle(hThread);
 
     ReleaseMutex(mx);
     CloseHandle(mx);
